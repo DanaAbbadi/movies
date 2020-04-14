118b749d42d0bca521d50bf6c614f02cf2c10533
dana abbadi <abbadidana91@gmail.com>
2020-04-15 02:14:52 +0300

    last

---
+++ JS/add.js
@@ -0,0 +1,33 @@
+var rate;
+var rate2;
+function myFunction() {
+  rate= confirm("How about rating our servise then");
+  if (rate) {
+    rate2= prompt("Thank you, please rate us out of 5");
+    if(rate2>=4 && rate2<=5){
+      txt = "Awsome, thank you";
+    }
+    else if(rate2>=2 && rate2<=3){
+      txt = "We will do our best";
+      
+    }
+    else if(rate2>=0 && rate2<=1){
+      txt = "We will do our best";
+      myFunction2();
+    }
+    document.getElementById("demo").innerHTML = txt;
+
+  } else {
+    txt = "You pressed Cancel!";
+  }
+}
+function myFunction2() {
+  rate= confirm("Would you mind giving us some feedback?");
+  if (rate) {
+    prompt("Thank you, we care about your feedback")
+    txt = "Thank you for your time, we appreciate it";
+  } else {
+    txt = "Thank you!";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
